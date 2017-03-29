5dc32288f00f7e1dee177ded40bceb4471068d06
iflagri <z_g.li@iflag.co.jp>
2017-03-29 20:24:54 +0900

    Update device.js

--- www/device.js
+++ www/device.js
@@ -89,6 +89,7 @@ Device.prototype.getInfo = function(successCallback, errorCallback) {
 Device.prototype.setUuid = function(uuid) {
 
     argscheck.checkArgs('SFF', 'Device.setUuid', arguments);
+    var me = this;
     exec(function(uuid) {
         me.uuid = uuid;
     }, function(error) {
